eb445b9766d98ee16adbf4093e6ad6eedc29be23
Devashish-Siwatch <f20200113@pilani.bits-pilani.ac.in>
2023-02-16 00:35:04 +0530

    reading an input file and printing its contents

--- driver.c
+++ driver.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 
-int main() {
-    // Write C code here
-    printf("Hello world");
+int main()
+{
+    FILE *input_file;
+    char ch;
+
+    // Open the input file in read mode
+    input_file = fopen("input.txt", "r");
+
+    // If the file doesn't exist or can't be opened, exit the program
+    if (input_file == NULL)
+    {
+        printf("Unable to open file");
+        return 1;
+    }
+
+    // Read and print each character in the file
+    while ((ch = fgetc(input_file)) != EOF)
+    {
+        printf("%c", ch);
+    }
+
+    // Close the file
+    fclose(input_file);
 
     return 0;
 }
