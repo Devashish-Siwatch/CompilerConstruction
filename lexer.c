0767c76badba11d3044820c0049a6b74a577a791
Devashish-Siwatch <f20200113@pilani.bits-pilani.ac.in>
2023-02-18 22:50:57 +0530

    added skeleton

--- lexer.c
+++ lexer.c
@@ -1,54 +1,13 @@
 #include <stdio.h>
-#define MAX_LEXEME_LENGTH 100
+#include "lexer.h"
 
-// List of valid token names in the language
-typedef enum valid_token_names
+Token get_next_token()
 {
-    ID,
-    NUM,
-    RNUM,
-    AND,
-    OR,
-    TRUE,
-    FALSE,
-    PLUS,
-    MINUS,
-    MUL,
-    DIV,
-    LT,
-    LE,
-    GT,
-    GE,
-    EQ,
-    NE,
-    DEF,
-    ENDDEF,
-    DRIVERDEF,
-    DRIVERENDDEF,
-    COLON,
-    RANGEOP,
-    SEMICOL,
-    COMMA,
-    ASSIGNOP,
-    SQBO,
-    SQBC,
-    BO,
-    BC,
-    COMMENTMARK
-} valid_token_names;
-
-// Defining token and its structure
-typedef struct token
+    Token t;
+    t.token_name = ID;
+    return t;
+}
+char get_next_char()
 {
-    valid_token_names token_name;
-    union
-    {
-        struct
-        {
-            char str[MAX_LEXEME_LENGTH];
-        } id;
-        int numeric_value;
-        double real_numeric_value;
-    };
-    int line_no;
-} Token;
+    return 'a';
+}
--- lexer.h
+++ lexer.h
@@ -1,6 +1,64 @@
 #include <stdio.h>
+#define MAX_LEXEME_LENGTH 100
 
+// List of valid token names in the language
+typedef enum valid_token_names
+{
+    ID,
+    NUM,
+    RNUM,
+    AND,
+    OR,
+    TRUE,
+    FALSE,
+    PLUS,
+    MINUS,
+    MUL,
+    DIV,
+    LT,
+    LE,
+    GT,
+    GE,
+    EQ,
+    NE,
+    DEF,
+    ENDDEF,
+    DRIVERDEF,
+    DRIVERENDDEF,
+    COLON,
+    RANGEOP,
+    SEMICOL,
+    COMMA,
+    ASSIGNOP,
+    SQBO,
+    SQBC,
+    BO,
+    BC,
+    COMMENTMARK
+} valid_token_names;
+
+// Defining token and its structure
+typedef struct token
+{
+    valid_token_names token_name;
+    union
+    {
+        struct
+        {
+            char str[MAX_LEXEME_LENGTH];
+        } id;
+        int numeric_value;
+        double real_numeric_value;
+    };
+    int line_no;
+} Token;
 FILE *getStream(FILE *fp);
 
+// removes comments from the testcase file and creates a new file without comments
 void removeComments(char *testcaseFile, char *cleanFile);
 
+// returns the next valid token from the file
+Token get_next_token();
+
+// returns the next character from the file
+char get_next_char();
