afddc7eef283d8d02ddf4948ec694b4f94a44d98
praneetk77 <praneetkarna77@gmail.com>
2023-02-25 13:20:28 +0530

    Func for unique terminals and updated first rule.

--- lexer.c
+++ lexer.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "lexer.h"
 #include "hashmap.h"
 // initializing the lexer variables
--- parser.h
+++ parser.h
@@ -3,12 +3,17 @@
 #include <stdio.h>
 #include "lexer.h"
 #define MAX_SIZE_TOKEN_NAME 100
-#define NUMBER_OF_RULES 127
+#define NUMBER_OF_RULES 128
 #define MAX_NUMBER_OF_UNIQUE_NONTERMINALS 100
 #define MAX_LENGTH_OF_NONTERMINAL 50
+#define MAX_NUMBER_OF_UNIQUE_TERMINALS 100
+#define MAX_LENGTH_OF_TERMINAL 50
+
 
 char arrayOfNonTerminals[MAX_NUMBER_OF_UNIQUE_NONTERMINALS][MAX_LENGTH_OF_NONTERMINAL];
+char arrayOfTerminals[MAX_NUMBER_OF_UNIQUE_TERMINALS][MAX_LENGTH_OF_TERMINAL];
 int number_of_unique_nonterminals;
+int number_of_unique_terminals;
 
 // typedef struct token_node
 // {
