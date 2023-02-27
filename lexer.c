4886457d35262dfaa91bb10a18ae95f402bf0667
C <chaitanyaiyer01@gmail.com>
2023-02-27 21:09:28 +0530

    Remove Comments

--- lexer.c
+++ lexer.c
@@ -740,3 +740,53 @@ void retract(int n)
         forward += BUFFER_SIZE;
     // printf("\n After retract(%d) begin is at : %d and forward is currently at : %d", n, begin, forward);
 }
+
+void removeComments(FILE *input_file_pointer, FILE *output_file_pointer)
+{
+    int state = 0;
+    char ch = fgetc(input_file_pointer);
+    while (ch != EOF)
+    {
+        if (state != 2)
+        {
+            if (ch == '*')
+            {
+                ch = fgetc(input_file_pointer);
+                if (ch == '*')
+                {
+                    state = 2;
+                }
+                else
+                {
+                    state = 0;
+                    fputc('*', output_file_pointer);
+                }
+            }
+            if (state == 0)
+            {
+                fputc(ch, output_file_pointer);
+            }
+        }
+        else if (state == 2)
+        {
+            if (ch == '*')
+            {
+                ch = fgetc(input_file_pointer);
+                if (ch == '*')
+                {
+                    state = 0;
+                }
+            }
+            else if (ch == EOF)
+            {
+                fputc(ch, output_file_pointer);
+                break;
+            }
+            else if (ch == '\n')
+            {
+                fputc(ch, output_file_pointer);
+            }
+        }
+        ch = fgetc(input_file_pointer);
+    }
+}
